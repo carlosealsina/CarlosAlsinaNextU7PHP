d3f5354f26c87fb0f4b345a05a4a29b7e25091ad
carlosealsina <47729495+carlosealsina@users.noreply.github.com>
2021-11-13 22:14:25 -0500

    verificar ingreso

--- codBase/server/check_login.php
+++ codBase/server/check_login.php
@@ -1,6 +1,27 @@
 <?php
 
+	require('conectarBD.php');
 
+	$con = new ConectorDB();
+	$msg = array();
+	$username = $_POST['username'];
+	$password = $_POST['password'];
+	
+	if($con->initConexion('calendario')=='OK'){
+		$result=$con->consultas(['usuarios'], ['email','contrasena'], "WHERE email='$username'");
 
+		while ($row = mysqli_fetch_array($result)) {
+			    if (password_verify($password, $row['contrasena'])){
+			    	$msg['email'] = $row['email'];
+			    	$msg['msg'] = 'OK';
+			    }else{
+			    	$msg['msg'] = 'Usuario o contraseña incorrectos';
+			    }
+			}
 
- ?>
+		echo json_encode($msg);
+	}else{
+		echo "error de conexión";
+	}
+
+?>
