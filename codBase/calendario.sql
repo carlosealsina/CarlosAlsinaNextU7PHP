b6706098d627d4d58518d95bc3dd1623a4007815
carlosealsina <47729495+carlosealsina@users.noreply.github.com>
2021-11-13 22:18:45 -0500

    Base de datos del proyecto

--- codBase/calendario.sql
+++ codBase/calendario.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 07-09-2021 a las 19:22:27
+-- Tiempo de generación: 14-11-2021 a las 04:17:33
 -- Versión del servidor: 10.1.37-MariaDB
 -- Versión de PHP: 5.6.39
 
@@ -32,12 +32,19 @@ CREATE TABLE `eventos` (
   `id_evento` int(11) NOT NULL,
   `fk_id_usuario` int(11) NOT NULL,
   `titulo` varchar(50) NOT NULL,
-  `fecha_inicio` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `hora_inicio` date DEFAULT NULL,
-  `fecha_finalizacion` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `hora_finalizacion` date DEFAULT NULL,
+  `fecha_inicio` date NOT NULL,
+  `hora_inicio` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `fecha_finalizacion` date NOT NULL,
+  `hora_finalizacion` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `todo_dia` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `eventos`
+--
+
+INSERT INTO `eventos` (`id_evento`, `fk_id_usuario`, `titulo`, `fecha_inicio`, `hora_inicio`, `fecha_finalizacion`, `hora_finalizacion`, `todo_dia`) VALUES
+(1, 1, 'evento 1', '2021-11-07', '2021-11-08 04:03:33', '2021-11-07', '2021-11-07 19:00:00', 0);
 
 -- --------------------------------------------------------
 
@@ -47,9 +54,9 @@ CREATE TABLE `eventos` (
 
 CREATE TABLE `usuarios` (
   `id_usuario` int(11) NOT NULL,
-  `email` varchar(20) NOT NULL,
+  `email` varchar(50) NOT NULL,
   `nombre` varchar(100) NOT NULL,
-  `contrasena` varchar(20) NOT NULL,
+  `contrasena` varchar(100) NOT NULL,
   `fecha_nacimiento` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -58,9 +65,9 @@ CREATE TABLE `usuarios` (
 --
 
 INSERT INTO `usuarios` (`id_usuario`, `email`, `nombre`, `contrasena`, `fecha_nacimiento`) VALUES
-(1, 'carlosealsina@gmail.', 'Carlos Eduardo Alsina Carrascal', '$2y$10$BdxF4EDfi7osS', '1980-02-07'),
-(2, 'castillolizeth7@gmai', 'Lizeth Catherine Castillo Tarazona', '$2y$10$1QmowkziTLGcT', '1986-04-07'),
-(3, 'carlos_e_alsina@hotm', 'Carlos Alsina', '$2y$10$fjilUGlNADIZ7', '1980-02-17');
+(1, 'carlosealsina@gmail.com', 'Carlos Eduardo Alsina Carrascal', '$2y$10$m1uQD1w37VnUqL2TRSRDSOhstbth.c/tI5gflHfCY8EYH/EJk1k3a', '1980-02-07'),
+(2, 'castillolizeth7@gmail.com', 'Lizeth Catherine Castillo Tarazona', '$2y$10$5cJVDC0PoFkNclv31KYE7uRSq4B1JarxHVm1TELH7MHn.cedO8gt.', '1986-04-07'),
+(3, 'carlos_e_alsina@hotmail.com', 'Carlos Alsina', '$2y$10$laiJwArEDBsBGqEMR8FMhOxnIz47.Wv2MNECeuOTWiqATFzcmWHse', '1980-02-17');
 
 --
 -- Índices para tablas volcadas
@@ -87,7 +94,7 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de la tabla `eventos`
 --
 ALTER TABLE `eventos`
-  MODIFY `id_evento` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_evento` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
