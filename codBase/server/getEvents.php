8021289555578654d50d7d1b72934134953ff512
carlosealsina <47729495+carlosealsina@users.noreply.github.com>
2021-11-16 17:01:23 -0500

    obtener eventos v2

--- codBase/server/getEvents.php
+++ codBase/server/getEvents.php
@@ -1,6 +1,21 @@
 <?php
   
+	require('conectarBD.php');
 
+	$con = new ConectorDB();
+	$eventos = array();
 
+	if($con->initConexion('calendario')=='OK'){
+		$result=$con->consultas(['eventos','usuarios'], ['*'],"WHERE usuarios.email='carlosealsina@gmail.com' AND usuarios.id_usuario=eventos.fk_id_usuario"); 
 
+		while ($row = mysqli_fetch_array($result)) {
+			    $eventos[] = $row;
+			}
+	
+	$eventos['msg'] = 'OK';	
+	$eventos['eventos'] = $eventos;
+	
+	echo json_encode($eventos);
+
+	}
  ?>
