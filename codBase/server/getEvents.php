bf58d9859bfde68a9ca38738e55ecbe00626c840
carlosealsina <47729495+carlosealsina@users.noreply.github.com>
2021-11-20 11:51:24 -0500

    obtener eventos v20 11 21

--- codBase/server/getEvents.php
+++ codBase/server/getEvents.php
@@ -9,11 +9,13 @@
 		$result=$con->consultas(['eventos','usuarios'], ['*'],"WHERE usuarios.email='carlosealsina@gmail.com' AND usuarios.id_usuario=eventos.fk_id_usuario"); 
 
 		while ($row = mysqli_fetch_array($result)) {
-			    $eventos[] = $row;
+			    $eventos['eventos']['title'] = $row['titulo'];
+			    $eventos['eventos']['start'] = $row['fecha_inicio'];
+			    $eventos['eventos']['end'] = $row['fecha_finalizacion'];
 			}
 	
 	$eventos['msg'] = 'OK';	
-	$eventos['eventos'] = $eventos;
+	//$eventos['eventos'] = $eventos;
 	
 	echo json_encode($eventos);
 
