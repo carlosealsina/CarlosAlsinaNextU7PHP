a08ce165ed2c003ebb37be091a816a0d91dec4af
carlosealsina <47729495+carlosealsina@users.noreply.github.com>
2021-11-20 19:05:47 -0500

    obtener eventos

--- codBase/server/getEvents.php
+++ codBase/server/getEvents.php
@@ -1,23 +1,30 @@
 <?php
   
 	require('conectarBD.php');
+	session_start();
 
-	$con = new ConectorDB();
-	$eventos = array();
-
-	if($con->initConexion('calendario')=='OK'){
-		$result=$con->consultas(['eventos','usuarios'], ['*'],"WHERE usuarios.email='carlosealsina@gmail.com' AND usuarios.id_usuario=eventos.fk_id_usuario"); 
-
-		while ($row = mysqli_fetch_array($result)) {
-			    $eventos['eventos']['title'] = $row['titulo'];
-			    $eventos['eventos']['start'] = $row['fecha_inicio'];
-			    $eventos['eventos']['end'] = $row['fecha_finalizacion'];
-			}
 	
-	$eventos['msg'] = 'OK';	
-	//$eventos['eventos'] = $eventos;
-	
-	echo json_encode($eventos);
 
-	}
+		$con = new ConectorDB();
+		$eventos = array();
+
+		if($con->initConexion('calendario')=='OK'){
+			$result=$con->consultas(['eventos','usuarios'], ['*'],"WHERE usuarios.email='". $_SESSION['username'] ."' AND usuarios.id_usuario=eventos.fk_id_usuario"); 
+			$i=0;
+			while ($row = mysqli_fetch_array($result)) {
+
+				    $eventos['eventos'][$i]['id']    = $row['id_evento'];
+	                $eventos['eventos'][$i]['title'] = $row['titulo'];
+	                $eventos['eventos'][$i]['start'] = $row['fecha_inicio'] . ' ' . $row['hora_inicio'];
+	                $eventos['eventos'][$i]['end']   = $row['fecha_finalizacion'] . ' ' . $row['hora_finalizacion'];
+	                $i++;
+				}
+
+		$eventos['msg'] = 'OK';	
+		
+		echo json_encode($eventos);
+
+		}
+
+	
  ?>
