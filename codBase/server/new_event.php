bc144c765e07d4e75226f0b118e88cb6dad7c533
carlosealsina <47729495+carlosealsina@users.noreply.github.com>
2021-11-24 23:14:00 -0500

    crear evento versión final

--- codBase/server/new_event.php
+++ codBase/server/new_event.php
@@ -1,5 +1,35 @@
 <?php
-  
 
+require 'conectarBD.php';
 
- ?>
+session_start();
+
+if (!isset($_SESSION['username'])) {
+	    header("Location:logout.php");
+	} else {
+
+		$id_usuario = $_SESSION['id'];
+
+		$con = new ConectorDB();
+		if($con->initConexion('calendario')=='OK'){
+
+		    $data['fk_id_usuario'] = "'" . $id_usuario . "'";
+		    $data['fecha_inicio'] = "'" . $_POST['start_date'] . "'";
+		    $data['hora_inicio']  = "'" . $_POST['start_hour'] . "'";
+		    $data['fecha_finalizacion'] = "'" . $_POST['end_date'] . "'";
+		    $data['hora_finalizacion'] = "'" . $_POST['end_hour'] . "'";
+		    $data['titulo'] = "'" . $_POST['titulo'] . "'";
+		    $data['todo_dia'] = "'" . $_POST['allDay'] . "'";
+
+		        if ($con-> insertData('eventos', $data)) {
+		            $response['msg'] = "OK";
+		        }
+
+		}
+
+}
+
+echo json_encode($response);
+
+$con->cerrarConexion();
+
