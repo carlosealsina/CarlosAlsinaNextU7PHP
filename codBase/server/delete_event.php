9ebafd9745c96e01ef6ba354b1a2cad3a16265c6
carlosealsina <47729495+carlosealsina@users.noreply.github.com>
2021-11-24 23:13:13 -0500

    Eliminar evento versión final

--- codBase/server/delete_event.php
+++ codBase/server/delete_event.php
@@ -1,5 +1,30 @@
 <?php
 
+	require 'conectarBD.php';
 
+session_start();
+
+if (!isset($_SESSION['username'])) {
+    header("Location:logout.php");
+}else{
+
+$campoID['id'] = "'" . $_POST['id'] . "'";
+
+$con = new ConectorDB();
+	if($con->initConexion('calendario')=='OK'){
+
+    $tabla = "eventos";
+    $condicion = "id_evento=" . $campoID['id'];
+
+        if ($con->eliminarRegistro($tabla, $condicion)) {
+            $response['msg'] = "OK";
+        } 
+
+	}
+echo json_encode($response);
+
+$con->cerrarConexion();
+
+}
 
  ?>
