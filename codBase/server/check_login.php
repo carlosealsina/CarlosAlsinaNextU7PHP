f1108977938629e6450f531169e6906826c2d0d0
carlosealsina <47729495+carlosealsina@users.noreply.github.com>
2021-11-24 23:11:25 -0500

    verificar usuario versión final

--- codBase/server/check_login.php
+++ codBase/server/check_login.php
@@ -8,12 +8,13 @@
 	$password = $_POST['password'];
 	
 	if($con->initConexion('calendario')=='OK'){
-		$result=$con->consultas(['usuarios'], ['email','contrasena'], "WHERE email='$username'");
+		$result=$con->consultas(['usuarios'], ['id_usuario', 'email','contrasena'], "WHERE email='$username'");
 
 		while ($row = mysqli_fetch_array($result)) {
 			    if (password_verify($password, $row['contrasena'])){
 			    	session_start();
 			    	$_SESSION['username'] = $username;
+			    	$_SESSION['id'] = $row['id_usuario'];
 			    	$msg['msg'] = 'OK';
 			    }else{
 			    	$msg['msg'] = 'Usuario o contraseña incorrectos';
