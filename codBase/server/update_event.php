1eb114e62e1d919bc7ba19c40b13e8abb61c40da
carlosealsina <47729495+carlosealsina@users.noreply.github.com>
2021-11-24 23:15:36 -0500

    actualizar evento versión final

--- codBase/server/update_event.php
+++ codBase/server/update_event.php
@@ -1,5 +1,34 @@
 <?php
  
+	require 'conectarBD.php';
+	session_start();
+	if (!isset($_SESSION['username'])) {
+	    header("Location:logout.php");
+	} else {
 
+		$_SESSION['username'];
+
+		$campoID['id'] = "'" . $_POST['id'] . "'";
+
+		$data['fecha_inicio'] = "'" . $_POST['start_date'] . "'";
+		$data['hora_inicio']  = "'" . $_POST['start_hour'] . "'";
+		$data['fecha_finalizacion']    = "'" . $_POST['end_date'] . "'";
+		$data['hora_finalizacion']     = "'" . $_POST['end_hour'] . "'";
+
+		$con = new ConectorDB();
+		if($con->initConexion('calendario')=='OK'){
+
+		    $tabla     = "eventos";
+		    $condicion = "id_evento=" . $campoID['id'];
+
+		        if ($con->actualizarRegistro($tabla, $data, $condicion)) {
+		            $response['msg'] = "OK";
+		        } 
+		} 
+
+		echo json_encode($response);
+
+		$con->cerrarConexion();
+	}
 
  ?>
